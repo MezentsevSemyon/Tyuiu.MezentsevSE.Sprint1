f402b26241a07c38870df77d81e4751d8b74b6ef
Tyuiu.MezentsevSE@outlook.com <Tyuiu.MezentsevSE@outlook.com>
2023-09-18 20:40:34 +0500

    Add project files.

---
+++ Tyuiu.MezentsevSE.Sprint1.Task0.V18.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MezentsevSE.Sprint1.Task0.V18.Lib;
+namespace Tyuiu.MezentsevSE.Sprint1.Task0.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+
+        }
+    }
+}
---
+++ Tyuiu.MezentsevSE.Sprint1.Task0.V18/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MezentsevSE.Sprint1.Task0.V18.Lib;
+
+namespace Tyuiu.MezentsevSE.Sprint1.Task0.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1| Выполнил: Мезенцев С.Е. | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил Мезенцев Семён Евгеньевич| ИИПб-23-2                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет 9*(6/2*3)/9/3                     *");
+            Console.WriteLine("* и выписывает результат на экран.                                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 9*(6/2*3)/9/3                                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+            
+            Console.ReadLine();
+        }
+    }
+}
