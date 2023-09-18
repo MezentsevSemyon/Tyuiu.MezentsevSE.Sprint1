58dac685b763fd7e742abca3892a787f5326127a
Tyuiu.MezentsevSE@outlook.com <Tyuiu.MezentsevSE@outlook.com>
2023-09-18 20:42:47 +0500

    Task0

--- Tyuiu.MezentsevSE.Sprint1.Task0.V18.Test/DataServiceTest.cs
+++ Tyuiu.MezentsevSE.Sprint1.Task0.V18.Test/DataServiceTest.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.MezentsevSE.Sprint1.Task0.V18.Test
         {
             DataService ds = new DataService();
             var res = ds.Calculate();
-            Assert.AreEqual(2, res);
+            Assert.AreEqual(3, res);
 
         }
     }
