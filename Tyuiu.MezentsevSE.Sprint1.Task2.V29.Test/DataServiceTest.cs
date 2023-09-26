234b47e1b3231cfa2e5813827cabb7f93dfd48dc
Tyuiu.MezentsevSE@outlook.com <Tyuiu.MezentsevSE@outlook.com>
2023-09-26 19:44:09 +0500

    Task2

---
+++ Tyuiu.MezentsevSE.Sprint1.Task2.V29.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MezentsevSE.Sprint1.Task2.V29.Lib;
+
+namespace Tyuiu.MezentsevSE.Sprint1.Task2.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 3600;
+            var res = ds.ConvertSecondsToHours(x);
+            Assert.AreEqual(1, res);
+
+        }
+    }
+}
